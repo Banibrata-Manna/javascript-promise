62a486e167b4a6dfae1654aba9b80d4d5ad45794
Banibrata Manna <banibratamanna@commons-MacBook-Air.local>
2025-09-08 22:22:36 +0530

    Added Study Links to README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Getting Started with Javascript Promises
+
+[Mozilla Documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise)
+
+[Practice Questions](https://medium.com/@francesco.saviano87/10-exercises-to-master-promises-in-javascript-hands-on-examples-with-detailed-explanations-b2cf4b948647)
--- promises.js
+++ promises.js
@@ -1,48 +1,48 @@
 // Create aSimple Prmise and resolve it after 2 seconds.
 
-// const a = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve({
-//             status: "Resolved",
-//             message: "This is real Slim Shady"
-//         });
-//     }, 2000);
-// });
+const a = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({
+            status: "Resolved",
+            message: "This is real Slim Shady"
+        });
+    }, 2000);
+});
 
 // Consume the promise
-// a.then((val) => {
-//     console.log(val);
-// });
+a.then((val) => {
+    console.log(val);
+});
 
-// console.log("An Immediate Log");
+console.log("An Immediate Log");
 
-// // Chaining Promises
-// const b = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve("I'm Slim Shady");
-//     }, 1000);
-// });
+// Chaining Promises
+const b = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("I'm Slim Shady");
+    }, 1000);
+});
 
-// const c = b.then((val) => {
-//     console.log(val);
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve("Yes I'm Real Shady");
-//         }, 1000);
-//     });
-// });
+const c = b.then((val) => {
+    console.log(val);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Yes I'm Real Shady");
+        }, 1000);
+    });
+});
 
-// c.then(
-//     (val) => {
-//         console.log(val);
-//         return new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//                 console.log("All you other Slim Shady's are just imitating, So wont the Real Slim Shady please stand up");
-//                 resolve();
-//             }, 1000);
-//         });
-//     }
-// )
+c.then(
+    (val) => {
+        console.log(val);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log("All you other Slim Shady's are just imitating, So wont the Real Slim Shady please stand up");
+                resolve();
+            }, 1000);
+        });
+    }
+)
 
 const d = '1';
 
